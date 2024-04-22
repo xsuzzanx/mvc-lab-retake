2f206ac5910cba0b2833496ba890812bea589d44
xsuzzanx <zuza.gry@gmail.com>
2024-04-22 13:04:26 +0200

    zadanie 2

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 const http = require('http');
-const carsModels = require('./cars');
-const htmlGen = require('./htmlGenerator');
 
+const routes = require('./routes/index');
 const PORT =3000;
 
-const requestListener = (req, res) =>{
-    const cars = carsModels.getCars();
-    console.log(cars);
-    res.setHeader("Content-Type", "text/html");
-    res.write(htmlGen.getHTMLDocumentStart());
-    res.write('<body>');
-    res.write(`<p>${carsModels.getCarInformation(1)}</p>`);
-    res.write(`<p>${carsModels.getCarAge(1)}</p>`);
-    res.write('</body>');
-    res.write(htmlGen.getHTMLDocumentEnd());
-    res.end();
+const requestListener = (req,res) =>{
+    if(req.url === '/' && req.method === 'GET'){
+        routes.handleHome(res);
+    }
+    else if(req.url === '/car' && req.method === 'GET')
+    {
+        routes.handleCar(res);
+    }
+    else if(req.url === '/add-car')
+    {
+        routes.handleAddCar(req.method, req, res);
+    }
+    else{
+        routes.handlePageNotFound(res);
+    }
 }
 
 const server = http.createServer(requestListener);
 
-server.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}.`);
-
+server.listen(PORT,() => {
+    console.log(`Server is running on  ${PORT}.`);
 });
---
+++ views/car.js
@@ -0,0 +1,36 @@
+function renderPage(data){
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+     <head>
+     <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Car</title>
+     </head>
+     <body>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/add-car">Add car</a>
+                    </li>
+                    <li>
+                        <a href="/car">Last added car</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <pres>${data}</pres>
+        </main>
+     </body>
+     </html>
+     `;
+};
+
+module.exports = {
+    renderPage
+};
